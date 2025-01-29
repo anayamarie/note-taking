d40d0e0d1e8498a96cecde5ff85c17c7e749b841
anayamarie <anaya.tingson@gmail.com>
2025-01-29 18:44:38 +0800

    Adjust port

--- api/server.ts
+++ api/server.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 // Initialize express app
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // MongoDB connection URI from environment variable
 const MONGODB_URI = process.env.MONGODB_URI || "";
